b3a8efe276a9159457111fa09feef01da4824552
Steve Ndengue <steve.ndengue@gmail.com>
2023-03-06 21:43:41 +0000

    Challenge: Average complete

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int somme=0;
+        for(int i=0; i<nums.length; i++){
+            somme+=nums[i];
+        }
+        double dblsum = somme;
+        double lennums = nums.length;
+        return dblsum/lennums;
     }
 }
